f7f1cc9c045d9d987b2dcd40654f5f9f33147799
MWorthy@appdev.yearup.org <MWorthy@appdev.yearup.org>
2023-10-25 10:56:09 -0400

    apply changes to project

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+"use strict"
+
+console.log("JS is loaded")
+
+let additionForm = document.getElementById("additionForm")
+additionForm.onsubmit = calculateSum
+
+function calculateSum (event) {
+    event.preventDefault()
+
+    console.dir(event.target)
+    let x = event.target.elements.leftOperand.valueAsNumber
+    let y = event.target.elements.rightOperand.valueAsNumber
+    console.log(x)
+    console.log(y)
+
+    let sum= (x) + (y)
+
+    let additionResults = document.getElementById("additionResults")
+     additionResults.innerHTML = `<h4> ${x} + ${y} = ${sum} <span class="display-result-red" </span> </h4>`
+}
+
+let addressForm = document.getElementById("addressForm")
+addressForm.onsubmit = addressResults
+
+function addressResults (event) {
+    event.preventDefault()
+
+    console.dir(event.target)
+    let city = event.target.elements.city.value
+    let state = event.target.elements.state.value
+    let country = event.target.elements.country.value
+    console.log(city)
+    console.log(state)
+    console.log(country)
+    
+    let submit= (city, state, country)  
+
+    let addressResults = document.getElementById("addressResults")
+     addressResults.innerHTML = `<h4> address:<span class="display-result-red"> ${city} , ${state}, ${country} </span> </h4>`
+
+}
+    
